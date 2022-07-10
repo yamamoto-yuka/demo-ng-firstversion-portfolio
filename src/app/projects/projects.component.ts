bdf2830aeb44877ff91ba5a8afa760245c6f635f
Yuka Yamamoto <yamamoto_yuka@hotmail.com>
2022-07-10 00:48:37 -0700

    created fileter screen

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,15 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  AfterViewInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
+export class ProjectsComponent implements OnInit, AfterViewInit {
+  @ViewChild('section', {static:true}) section:ElementRef<HTMLDivElement>;
+  @ViewChildren('imgWrap',{ read: ElementRef }) images:QueryList<ElementRef>;
+  isCategoryActive:boolean = false;
+  active:boolean =true;
+  inActive:boolean = false;
 
-  constructor() { }
+  projects: any[] = [
+    {
+      title: '美稀, Miki',
+      roles: [
+        { role: 'Branding' },
+        { role: 'Design' },
+        { role: 'Development' },
+        { role: 'Management' },
+      ],
+      skills: [
+        { skill: 'Angular' },
+        { skill: 'Strapi' },
+        { skill: 'Illustrator' },
+        { skill: 'Figma' },
+      ],
+      img: '../../assets/projects/miki.jpg',
+    },
+    {
+      title: '美稀, Miki',
+      roles: [
+        { role: 'Branding' },
+        { role: 'Design' },
+        { role: 'Development' },
+        { role: 'Management' },
+      ],
+      skills: [
+        { skill: 'Angular' },
+        { skill: 'Strapi' },
+        { skill: 'Illustrator' },
+        { skill: 'Figma' },
+      ],
+      img: '../../assets/projects/miki.jpg',
+    }
+  ];
+
+
+  constructor() {}
+
+
+
+
+  categoryBtn(){
+    this.isCategoryActive = !this.isCategoryActive;
+    this.active = !this.active;
+    this.inActive = !this.inActive;
+  }
+
+
 
   ngOnInit(): void {
+    // console.log(this.projects[0].roles[0]);
+ 
+
+
   }
+
+  ngAfterViewInit(): void {
+
+  }
+
 
 }
